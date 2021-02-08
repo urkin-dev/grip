83805ae21d46ec9647dd7f8fef3590e211d19245
Ivan <yurkin.ivan5456@gmail.com>
2021-02-08 11:19:55 +0500

    Added about us  section and about us page

---
+++ src/components/AboutSection.js
@@ -0,0 +1,31 @@
+import home1 from '../img/home1.png'
+
+export default function AboutSection() {
+	return (
+		<div className="about-section">
+			<div className="description">
+				<div className="title">
+					<div className="hide">
+						<h2>We work to make</h2>
+					</div>
+					<div className="hide">
+						<h2>
+							your <span>dreams</span> come
+						</h2>
+					</div>
+					<div className="hide">
+						<h2>true</h2>
+					</div>
+				</div>
+				<p>
+					Contact us for any photography or videography ideas that you have. We
+					have professionals with amazing skills
+				</p>
+				<button>Contact Us</button>
+			</div>
+			<div className="image">
+				<img src={home1} alt="Guy with a camera" />
+			</div>
+		</div>
+	)
+}
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,9 @@
+import AboutSection from '../components/AboutSection'
+
+export default function AboutUs() {
+	return (
+		<div>
+			<AboutSection />
+		</div>
+	)
+}
